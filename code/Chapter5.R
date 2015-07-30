7ebfd6ce8027e812ef651e96ca74c19ef894af27
jfortuny <jfortuny@jfortuny.com>
2015-07-30 12:59:04 -0400

    added chapter 5 and tidyR demo data and code

---
+++ code/Chapter5.R
@@ -0,0 +1,20 @@
+# Chapter 5
+SalarySurvey_p130 <- read.delim("~/R Projects/AliHadiRegressionByExample/All_Data/P130.txt")
+View(SalarySurvey_p130)
+attach(SalarySurvey_p130)
+
+# use dplyr to implement the %>% pipeline
+# use tidyR to reshape the data
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# prepare the data for plotting with ggplot2's boxplots
+salNarrow = gather(SalarySurvey_p130, variable, value)
+ggplot(data = salNarrow[salNarrow$variable=="S",], aes(x = variable, y = value)) + 
+              geom_boxplot() + labs(title = "Salary")
+ggplot(data = salNarrow[salNarrow$variable=="X",], aes(x = variable, y = value)) + 
+              geom_boxplot() + labs(title = "Experience")
+ggplot(data = salNarrow[salNarrow$variable=="E",], aes(x = variable, y = value)) + 
+              geom_boxplot() + labs(title = "Education")
+# ggplot(data = salNarrow[salNarrow$variable=="M",], aes(x = variable, y = value)) + geom_boxplot()
